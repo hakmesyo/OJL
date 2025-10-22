752dd8b317be3a4a3d10cc699bf6b1dfcf078289
dell_lab <dell_lab@DESKTOP-QD1AFH4>
2025-10-22 17:32:16 +0300

    LLM Tools ismini Project X-Ray olarak değiştirdik.
    Artık proje milyonlarca satır dahi olsa tıkanmadan concat edip
    dosyaya b. yazabiliyor. Bir de project tree-view structure a da mtodlar
     da eklenebiliyor

--- src/main/java/jazari/app/Jazo.java
+++ src/main/java/jazari/app/Jazo.java
@@ -50,7 +50,7 @@ public class Jazo extends javax.swing.JFrame {
     public Jazo() {
         initComponents();
         btn_open.setTransferHandler(new ImageTransferHandler());
-        this.setTitle("Open Jazari Library (Jazari Annotation Tool)  [21.10.2025]");
+        this.setTitle("Open Jazari Library (Jazari Annotation Tool)  [22.10.2025]");
         setLocationRelativeTo(null);
     }
 
